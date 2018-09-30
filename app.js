8416e0a138f1d06d452971ae393fdd2f78d25e8e
Louis Riehl <louisriehl@gmail.com>
2018-09-30 17:31:07 +0000

    fixed === error in app, closed <a> tag in home

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-/* app.js */
-
 //grab express
 var app = require('express')();
 
@@ -12,25 +10,30 @@ var posts = [
     body: 'Blog post number 1'
   },
   {
-    id: '2',
+    id: 2,
     author: 'Drake',
     title: 'Express: Starting From The Bottom',
     body: 'Blog post number 2'
   },
   {
-    id: '3',
+    id: 3,
     author: 'Emma',
     title: 'Streams',
     body: 'Blog post number 3'
   },
   {
-    id: '4',
+    id: 4,
     author: 'Cody',
     title: 'Events',
     body: 'Blog post number 4'
+  },
+  {
+    id: 5,
+    author: 'Louis',
+    title: 'Success!',
+    body: 'Blog post number 5'
   }
-
-];
+]
 
 //set the view engine to EJS
 app.set('view engine', 'ejs');
@@ -38,23 +41,23 @@ app.set('view engine', 'ejs');
 //blog home page
 app.get('/', function (req, res) {
   //render `home.ejs` with list of posts
-  res.render('home',{posts: posts});
-});
+  res.render('home', { posts: posts })
+})
 
 //blog post
 app.get('/post/:id', function(req, res) {
   //find the post in the posts array
   var post = posts.filter(function(post) {
-    return post.id === req.params.id;
+    return post.id == req.params.id;
   })[0];
 
   //render the post.ejs template with the post content
-  res.render(post, {
+  res.render('post', {
     author: post.author,
     title: post.title,
-    body: post.body,
-  });
-});
+    body: post.body
+  })
+})
 
 app.listen(8080);
 
